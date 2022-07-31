8dac64db0d02f2a2c6c3f53a80ffd12ba23ff3e0
JustDankas <jan-haspel@hotmail.com>
2022-07-31 14:04:00 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import words from './words.json'
 import './App.css';
 import Grid from './Grid';
 import GameOver from './GameOver';
-
+import {BsFillBackspaceFill} from 'react-icons/bs'
 
 const words_with_5_letters = words.filter(word=>word.length==5);
 const initRound = 0
@@ -11,14 +11,28 @@ const defaultMatrix = new Array(6).fill(0)
 .map(row=>new Array(5).fill(''));
 const defaultClassMatrix = new Array(6).fill(0)
 .map(row=>new Array(5).fill('cell'));
+
+const lettersArray = ['q','w','e','r','t','y','u','i','o','p',
+'a','s','d','f','g','h','j','k','l','Enter',
+'z','x','c','v','b','n','m','Backspace'];
+
+const lettersRowArray = [['q','w','e','r','t','y','u','i','o','p'],
+['a','s','d','f','g','h','j','k','l'],['Enter',
+'z','x','c','v','b','n','m','Backspace']];
+
+const initLettersObj = {}
+lettersArray.forEach((letter)=>letter=='Enter' || letter=='Backspace'?
+initLettersObj[letter]=letter:initLettersObj[letter]='undefined')
+
 function App() {
 
   const [lettersMatrix,setLettersMatrix] = useState(defaultMatrix);
   const [classMatrix,setClassMatrix] = useState(defaultClassMatrix);
   const [round,setRound] = useState(initRound)
-  const [selectedKeys,setSelectedKeys] = useState();
+  const [chosenLetters,setChosenLetters] = useState(initLettersObj);
   const [word,setWord] = useState(randomWord());
   const [gameState,setGameState] = useState('')
+
 
 
   const renderMatrix = lettersMatrix
@@ -26,6 +40,11 @@ function App() {
     word.map((letter,indx)=>{
     return <div key={indx} id={row*6+indx} className={classMatrix[row][indx]}>{letter}</div>
   }))
+
+  function HandleClickLetter(letter){
+      handleKeyPress(letter)
+
+  }
 
   function handleRestart(){
       setLettersMatrix(new Array(6).fill(0)
@@ -35,10 +54,20 @@ function App() {
       setRound(initRound)
       setWord(randomWord())
       setGameState('')
+      setChosenLetters(initLettersObj)
   }
 
-  function handleKeyPress({key,code}){
-    console.log(word)
+  function handleKeyPress(param){
+    let key
+    let code
+    if(typeof param === "object" && !Array.isArray(param)){
+      key = param.key
+      code = param.code
+    }
+    else{
+      key=param
+      code=param
+    }
     const matrix = [...lettersMatrix];
     const classesMatrix = [...classMatrix];
     const currentRow = matrix[round];
@@ -72,26 +101,40 @@ function App() {
       if(freeSpot<0){
         if(!words_with_5_letters.includes(currentRow.join(''))) return
 
+        const chosenLettersObj = Object.assign(chosenLetters)
+
         currentRow.forEach((letter,index)=>{
           let style = ''
-          word.charAt(index)==letter?style="cell correct":
-          !word.includes(letter)?style="cell incorrect":
-          currentRow.indexOf(letter)==currentRow.lastIndexOf(letter)?style="cell different":
-          word.indexOf(letter)==word.lastIndexOf(letter)?style="cell different":
-          style="cell incorrect"
-          // classesMatrix[round][index]=style
-          style=="cell incorrect"?style="cell incorrect flip-incorrect":
-          style=="cell correct"?style="cell correct flip-correct":
-          style="cell different flip-different"
+          if(word.charAt(index)==letter) style="cell correct flip-correct"
+          else if(!word.includes(letter)) style="cell incorrect flip-incorrect"
+          else if(word.indexOf(letter)==word.lastIndexOf(letter) && currentRow.indexOf(letter)==index) style="cell different flip-different"
+          else if(word.indexOf(letter)!=word.lastIndexOf(letter)) style="cell different flip-different"
+          else style="cell incorrect flip-incorrect"
           setTimeout(() => {
             const el = document.getElementById(round*6+index)
             el.className=style
+            if(style=="cell incorrect flip-incorrect"){
+              if(chosenLettersObj[letter]=='undefined'){
+                chosenLettersObj[letter]='incorrect'
+              }
+            }
+            else if(style=="cell different flip-different"){
+              if(chosenLettersObj[letter]!='correct'){
+                chosenLettersObj[letter]='different'
+              }
+            }
+            else{
+              chosenLettersObj[letter] = 'correct'
+            }
           }, 175*index);
         })
 
 
         setTimeout(() => {
           setClassMatrix(classesMatrix)
+
+          setChosenLetters(chosenLettersObj)
+          
         }, 1000);
 
         if(currentRow.join('')==word){
@@ -121,7 +164,24 @@ function App() {
       <div className="matrix">
         {renderMatrix}
       </div>
-      <GameOver gameState={gameState} attempts={round} handleRestart={()=>handleRestart()} />
+      <div className="letters-c">
+        {lettersRowArray.map((row)=>(
+          <div className="row-c">
+            {row.map((letter,index)=>(
+                <button key={index} 
+                className={chosenLetters[letter]=='Enter'?"enter-btn":
+                chosenLetters[letter]=='Backspace'?"backspace-btn": 
+                chosenLetters[letter]=='undefined'?"letter":
+                chosenLetters[letter]=="correct"?"letter correct":
+                chosenLetters[letter]=="incorrect"?"letter incorrect":
+                "letter different"
+              } 
+                onClick={()=>HandleClickLetter(letter)}>{letter=="Backspace"?<BsFillBackspaceFill/>:letter}</button>
+            ))}
+          </div>
+        ))}
+      </div>
+      <GameOver word={word} gameState={gameState} attempts={round} handleRestart={()=>handleRestart()} />
     </div>
   );
 }
--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -1,15 +1,19 @@
 import React, { Component, useEffect } from 'react';
-function GameOver({gameState,attempts,handleRestart}) {
+function GameOver({word,gameState,attempts,handleRestart}) {
     useEffect(()=>{
         console.log(gameState,attempts)
     })
     return ( 
-        <div className={gameState=='won'?"game-over won":
+      <div 
+      className={gameState=='won'?"game-over won":
       gameState=='lost'?"game-over lost":
       "game-over hidden"
-    }>
+      }>
         <h1>{gameState=='won'?"Won":"Lost"}</h1>
-        <div className="attempts">Turns: {attempts+1}</div>
+        <div className="info">
+          <div className="word">Word: {word}</div>
+          <div className="attempts">Turns: {attempts+1}</div>
+        </div>
         <button className="restart" onClick={()=>handleRestart()}>Restart</button>
       </div>
      );
